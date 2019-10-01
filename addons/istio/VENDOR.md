87e6b1d72b74e789ab6df1a4a96c236b2eaf721f
Salim Badakhchani <sbadakhc@gmail.com>
2019-10-01 16:58:05 +0200

    add istio and monitoring addons

---
+++ addons/istio/VENDOR.md
@@ -0,0 +1,1 @@
+Provenance: Charts vendored from: Istio 1.2.4
---
+++ addons/istio/install-istio.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+declare -r here="$(dirname "$(pwd -P)/${BASH_SOURCE[0]}")"
+pushd "$here"
+
+kubectl apply -f namespace.yaml
+
+helm template istio-init --name istio-init --namespace istio-system | kubectl apply -f -
+
+declare crd_count
+while true
+do
+    crd_count=$(echo $(kubectl get crds | grep 'istio.io' | wc -l))
+    echo "CRDs: $crd_count"
+    if [ $crd_count -eq 23 ]
+    then
+        echo "OK."
+        break
+    else
+        echo "Waiting..."
+    fi
+    sleep 1
+done
+
+helm template \
+    --name istio \
+    --namespace istio-system \
+    --values values-esqimo.yaml \
+    istio \
+    | kubectl apply -f -
+
+kubectl label namespace default --overwrite=true istio-injection=enabled
+
+declare phase
+while true
+do
+    phase=$(kubectl get -n istio-system pod -l app=sidecarInjectorWebhook -o jsonpath='{.items[0].status.phase}')
+    echo "Webhook phase: $phase"
+    if [[ $phase == "Running" ]]
+    then
+        echo "OK."
+        break
+    else
+        echo "Waiting..."
+    fi
+    sleep 1
+done
+
+popd
